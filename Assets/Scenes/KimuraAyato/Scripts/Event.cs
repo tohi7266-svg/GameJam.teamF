52de291d603d09c9b27e84b4cefd91ec0454b8fd
kimuraayato6775-lgtm <kimura.ayato.6775@gmail.com>
2026-06-24 05:27:22 +0900

    スクリプト「Event」を追加しMain Programから引用・編集。空のオブジェクト「EVENT」を作成し「Event」スクリプトを付与した。

---
+++ Assets/Scenes/KimuraAyato/Scripts/Event.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class Event : MonoBehaviour
+{
+    public Image Image;
+    public GameObject STORY;//選択肢・テキストボックスの表示の操作のためのパーツ。選択肢などを中に入れる。
+    public Text StoryProgression;//物語を表示するテキストボックス
+    //画像
+    public Sprite title;
+
+
+
+    //選択肢
+    public GameObject Select;//選択肢の表示の操作のためのパーツ。選択肢を中に入れる。
+    public GameObject SelAns;//選択中背景
+    public Text Options1;
+    public Text Options2;
+    public Text Options3;
+
+    //変数
+    public int StandardizedScore = 0;//偏差値
+    public int Favorability = 0;//好感度
+
+    private int page = 0;//ページ番号
+    private int SelNo = 0;//選択中背景
+    private Image img;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        img = Image.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))//もしエンターキーが押されたなら
+        {
+            page++;//ページを1足す
+
+        }
+        switch (page)
+        {
+            case 0:
+                img.sprite = title;
+                STORY.SetActive(false);
+                break;
+            case 1:
+                Select.SetActive(false);
+                STORY.SetActive(true);
+                StoryProgression.text = "今日は高校最後の年の始業式。\n学年が変わるということはクラスも変わる。";
+                break;
+
+            case 2:
+
+
+
+                break;
+
+        }
+    }
+}
